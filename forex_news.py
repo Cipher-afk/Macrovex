5bbf6ed84c05bfe251fa8ed2fd3bb4e15c805da0
Cipher <ajaokudos34@gmail.com>
2026-09-01 16:32:52 +0100

    added teh feed parser in the function

--- forex_news.py
+++ forex_news.py
@@ -6,12 +6,13 @@ from pathlib import Path
 from typing import Dict, List
 from pprint import pprint
 
-feed = feedparser.parse("https://www.fxstreet.com/rss/news")
 HIGH_IMPACT_KEYWORD = []
 JSON_FILE = Path(BASE_DIR, "calendar.json")
 
 
 def get_all_news():
+    feed = feedparser.parse("https://www.fxstreet.com/rss/news")
+    print("bozo_exception",feed.get("bozo_exception"),flush=True)
     news_list = []
     for entry in feed.entries:
         output = f"Title: {entry.title}\nSummary: {entry.summary}\nLink: {entry.link}\n"
@@ -94,7 +95,7 @@ if __name__ == "__main__":
     # day, month = date.day, datetime.strftime(date, "%b")
     # day += 1
     # todays_events = filter(lambda x: f"{month} {day}" in x, get_high_impact_events())
-    pprint(get_all_news()[0])
+    pprint(get_all_news())
     # # today = f"{month} {day}"
     # while True:
     #     todays_events = filter(
