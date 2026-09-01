165cf92edfaf9195fb26467185e1e1df904713b0
Cipher <ajaokudos34@gmail.com>
2026-09-01 16:42:33 +0100

    added some things

--- forex_news.py
+++ forex_news.py
@@ -15,7 +15,8 @@ def get_all_news():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     r = requests.get("https://www.fxstreet.com/rss/news", headers=headers)
     feed = feedparser.parse(r.content)
-    # print(r.content,flush=True)
+    print(r.status_code,flush=True)
+    print(r.content,flush=True)
     news_list = []
     for entry in feed.entries:
         output = f"Title: {entry.title}\nSummary: {entry.summary}\nLink: {entry.link}\n"
